96357addfd6add2a34a07e3e6ec5aa8513e00fed
Akshay Jadhav <133483587+akshayjadhav-1111@users.noreply.github.com>
2023-12-24 15:40:27 +0530

    Added solution - LeetHub

---
+++ Alternate positive and negative numbers - GFG/alternate-positive-and-negative-numbers.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	   int temp[n];
+	   int pos=0,neg=0;
+	   int i=0;
+	   
+	   while(pos<n||neg<n){
+	       
+	       
+	       while(arr[pos]<0 &&pos<n){
+	           pos++;
+	            
+	       }
+	       if(pos<n){
+	           
+	       
+	       temp[i]=arr[pos];
+	       pos++;
+	       i++;
+	       }
+	       
+	       while(arr[neg]>=0 && neg<n ){
+	           neg++;
+	       }
+	       
+	       if(neg<n){
+	       temp[i]=arr[neg];
+	       neg++;
+	       i++;
+	       }
+	       
+	   }
+	   for(int i =0;i<n;i++){
+	       arr[i]=temp[i];
+	   }
+	   
+	   
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
